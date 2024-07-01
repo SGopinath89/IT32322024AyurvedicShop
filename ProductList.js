922da26ed8894765b29c68beec8d197c490445af
SandunikaT <154103283+SandunikaT@users.noreply.github.com>
2024-07-01 08:03:02 +0530

    create product List

---
+++ ProductList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ProductList = ({ products, addToCart }) => {
+  return (
+    <div>
+      <h2>Products</h2>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <h3>{product.name}</h3>
+            <p>Price: ${product.price}</p>
+            <button onClick={() => addToCart(product)}>Add to Cart</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
