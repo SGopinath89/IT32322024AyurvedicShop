43f74be168d590117b3a501e9677c0e565c41311
SandunikaT <154103283+SandunikaT@users.noreply.github.com>
2024-07-01 08:01:17 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import NavBar from './NavBar';
+import React, { useState } from 'react';
+import ProductList from './ProductList';
+import Cart from './Cart';
 
 function App() {
+  const [products] = useState([
+    { id: 1, name: 'Ashwagandha', price: 10.99 },
+    { id: 2, name: 'Triphala', price: 15.99 },
+    { id: 3, name: 'Brahmi', price: 12.99 },
+  ]);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+  const removeFromCart = (index) => {
+    setCart(cart.filter((item, i) => i !== index));
+  };
   return (
     <div className="App">
       <NavBar></NavBar>
+    <div className="content">
+        <ProductList products={products} addToCart={addToCart} />
+        <Cart cart={cart} removeFromCart={removeFromCart} />
+      </div>
     </div>
   );
 }
